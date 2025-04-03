682ef4351027a9f3d7e264f15ec513fc16a1b58d
Letícia Schiavon <leticiaschiavon20@gmail.com>
2025-04-03 17:09:28 -0400

    feat: migrations users

--- src/shared/typeorm/migrations/1743713019893-CreateUsers.ts
+++ src/shared/typeorm/migrations/1743713019893-CreateUsers.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateUsers1742932884785 implements MigrationInterface {
+export class CreateUsers1743713019893 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
